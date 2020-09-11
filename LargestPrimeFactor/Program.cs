5e013afe7e53804d3ec18c02afc3994d5aa0bbe8
Nick Bowman <42209122+NJBowman@users.noreply.github.com>
2020-09-11 11:33:45 +0100

    Prime method usage

--- LargestPrimeFactor/Program.cs
+++ LargestPrimeFactor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LargestPrimeFactor
 {
@@ -6,7 +8,52 @@ namespace LargestPrimeFactor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program p = new Program();
+        }
+
+        private bool isPrime(double number)
+        {
+            if (number == 2)
+            {
+                return true;
+            }
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public double LargestPrimeFactor(double num)
+        {
+            List<double> primeFactors = new List<double>();
+            num = 13195;
+
+            //2 is the first prime number
+            double div = 2;
+
+            while (div < num)
+            {
+                if (num % div == 0)
+                {
+                    if (primeFactors.Contains(div) == false)
+                    {
+                        primeFactors.Add(div);
+                    }
+
+                    num /= div;
+                }
+                div++;
+            }
+
+
+
+            
+            return primeFactors.Max();
         }
     }
 }
